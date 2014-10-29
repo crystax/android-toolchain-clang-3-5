1acec286e480e0c657b6a394cad152e35843e4f9
Dmitry Moskalchuk <dm@crystax.net>
2014-10-30 02:03:13 +0300

    Add -lstdc++ after -lcrystax to linker specs

--- lib/Driver/Tools.cpp
+++ lib/Driver/Tools.cpp
@@ -7633,6 +7633,7 @@ void gnutools::Link::ConstructJob(Compilation &C, const JobAction &JA,
               CmdArgs.push_back("-lcrystax");
               CmdArgs.push_back("-Bdynamic");
           }
+          CmdArgs.push_back("-lstdc++");
           CmdArgs.push_back("-lm");
       }
 
